ecd45618e09419ab93ab20c9c3ecfc544e05e958
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-04-05 10:18:28 +0200

    exercises 34-35

---
+++ JS/E35-callbacks.js
@@ -0,0 +1,17 @@
+/* Write a function called repeatHello which takes a callback as it's parameter. 
+The callback function will simply print "Hello". The repeatHello function will have to 
+execute the callback function with an interval of 1 second. The callback function must 
+be an arrow function, can you also explain why? */
+
+function repeatHello(callback) {
+    setTimeout(callback, 1000)
+}
+
+repeatHello(() => {
+  console.log('hello')
+});
+
+/*
+Le arrow functions hanno alcuni vantaggi rispetto alle funzioni regolari, 
+come il collegamento automatico allo scope genitore e un return 
+implicito se il corpo della funzione è una singola espressione. */
