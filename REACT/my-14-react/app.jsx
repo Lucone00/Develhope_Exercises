6b5b610c6d013d9ec95859c4697210c88fde69a2
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-05-02 10:29:16 +0200

    exercise 14

---
+++ REACT/my-14-react/app.jsx
@@ -0,0 +1,14 @@
+import Welcome from "./components/welcome";
+import Counter from "./components/counter";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Luca" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
