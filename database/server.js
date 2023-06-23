80826767f6496d5f358665e0e4c139bd3dc54836
Lucone00 <luca.giuntoli2001.b@gmail.com>
2023-06-23 15:08:38 +0200

    exercise 11

--- database/server.js
+++ database/server.js
@@ -8,7 +8,7 @@ const createTable = async () => {
   await db.none(`DROP TABLE IF EXISTS mydatabase;
   CREATE TABLE users(
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL
+    name VARCHAR(20) NOT NULL,
   )`);
   await db.none(`INSERT INTO users(name) VALUES('Manfredi)`);
   await db.none(`INSERT INTO users(name) VALUES('Luca)`);
@@ -16,6 +16,25 @@ const createTable = async () => {
 };
 
 createTable();
+
+const getAllPlanets = async (req, res) => {
+  const planets = await db.many("SELECT * FROM planet;");
+  res.status(200).send(planets);
+};
+
+const getSinglePlanet = async (req, res) => {
+  const { id } = req.params;
+  const planet = await db.one(`SELECT * id WHERE ID = ${id}`);
+  res.send(planet);
+};
+
+const createPlanet = async (req, res) => {
+  const name = req.body.name;
+  await db.none(`INSERT INTO planets (name) VALUES ($1) ($2)`, [name]);
+  res.status(201).send("Planet creation successfully");
+};
 app.listen(3000, () => {
   console.log("listening");
 });
+
+module.exports = {getAllPlanets, getSinglePlanet, createPlanet}
